cb49dc91907bfe5e36d9f88bee0dbccfcc6ebf39
Rekss13 <rekss13@gmail.com>
2022-06-20 23:41:49 +0300

    Serialization test

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace MyMessanger
 {
@@ -6,8 +7,16 @@ namespace MyMessanger
     {
         static void Main(string[] args)
         {
-            Message message = new Message();
-            Console.WriteLine(message.ToString());
+            Message message = new Message("RusAl", "Privet", DateTime.UtcNow);
+
+            string serializedMessage = JsonConvert.SerializeObject(message);
+            Console.WriteLine(serializedMessage);
+
+            Message deserializedMessage = JsonConvert.DeserializeObject<Message>(serializedMessage);
+            Console.WriteLine(deserializedMessage);
+
+            //{ "UserName":"RusAl","MessageText":"Privet","TimeStamp":"2022-06-20T20:38:43.0408771Z"}
+            //RusAl < 20.06.2022 20:38:43 >: Privet
         }
     }
 }
